38ef7ae7d200ae782f61b7e4f215b246be17de4d
amirbalochsowaan <amir.baloch@ahwaal.com>
2026-06-12 17:19:04 +0500

    ignore permission again

--- ecommerce_integrations/amazon/doctype/amazon_sp_api_settings/amazon_sp_api_settings.py
+++ ecommerce_integrations/amazon/doctype/amazon_sp_api_settings/amazon_sp_api_settings.py
@@ -31,8 +31,8 @@ class AmazonSPAPISettings(Document):
 		elif self.max_retry_limit and self.max_retry_limit > 5:
 			frappe.throw(frappe._("Value for <b>Max Retry Limit</b> must be less than or equal to 5."))
 
-	def save(self):
-		super(AmazonSPAPISettings, self).save()
+	def save(self, *args, **kwargs):
+		super(AmazonSPAPISettings, self).save(*args, **kwargs)
 
 		if not self.is_old_data_migrated:
 			migrate_old_data()
